6997f0c5de4cb0e8b355e166212698f3f3b725a1
HostAzul <contato@hostazul.com.br>
2022-12-01 14:26:18 -0300

    melhoria para facilitar o uso

--- EnvioEmailExemplo1/Program.cs
+++ EnvioEmailExemplo1/Program.cs
@@ -8,18 +8,25 @@ namespace EnvioEmailExemplo1
         static void Main(string[] args)
         {
             Console.WriteLine("Início do envio de e-mail!");
+
+            var para = "email_destinatario";
+            var meuEmail = "meu_email";
+            var senhaMeuEmail = "senha_meu_email";
+            var porta = 587;
+            var enderecoSMTP = "endereco_smtp";
+
             try
             {
                 MailMessage mm = new MailMessage();
-                mm.To.Add("meuemail@gmail.com");
+                mm.To.Add(para);
                 mm.Subject = "Teste de Envio";
                 mm.Body = "Teste de envio de e-mail por aplicação SMTP";
                 mm.IsBodyHtml = true;
-                mm.From = new MailAddress("suaconta@seudominio.com.br");
+                mm.From = new MailAddress(meuEmail);
 
-                SmtpClient smtp = new SmtpClient("mail.seudominio.com.br");
-                smtp.Port = 587;
-                smtp.Credentials = new System.Net.NetworkCredential("suaconta@seudominio.com.br", "sua_senha");
+                SmtpClient smtp = new SmtpClient(enderecoSMTP);
+                smtp.Port = porta;
+                smtp.Credentials = new System.Net.NetworkCredential(meuEmail, senhaMeuEmail);
 
                 smtp.Send(mm);
 
